566f859e07c98841180c263ffa8787cb45ad4665
David Slutsky <21009377+davidsl4@users.noreply.github.com>
2023-10-26 20:18:03 +0300

    Update iron-swords-ur-sig.user.js

--- docs/iron-swords-ur-sig.user.js
+++ docs/iron-swords-ur-sig.user.js
@@ -1,16 +1,22 @@
 // ==UserScript==
 // @name         WME Iron Swords Signature
+// @namespace    http://tampermonkey.net/
 // @version      0.1
 // @description  try to take over the world!
-// @author       Israeli Community
+// @author       Israel
 // @match        https://*.waze.com/*editor*
 // @match        https://waze.com/*editor*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=waze.com
 // ==/UserScript==
 
-const SIGNATURE = '\n\n🇮🇱 ביחד ננצח 🇮🇱';
+const BLANK_LINES = 2;
+const SIGNATURE = '🇮🇱 ביחד ננצח 🇮🇱';
+
 
 async function init() {
+  const BLANK_LINES_CONTENT = new Array(BLANK_LINES).fill('\n').join('');
+  const FULL_SIG = BLANK_LINES_CONTENT + SIGNATURE;
+  
   function mockImplementation(object, fnName, handler) {
     const originalFunction = object[fnName];
     object[fnName] = function(...args) {
@@ -18,18 +24,35 @@ async function init() {
     }
   }
 
+  function mockFetchRequest(url, handler) {
+    mockImplementation(unsafeWindow, 'fetch', ([resource, ...restArgs], originalFetch) => {
+      if (resource instanceof Request) {
+        const rurl = new URL(resource.url);
+        if (rurl.pathname === url) {
+          const newArgs = handler(resource, ...restArgs);
+          return originalFetch.apply(unsafeWindow, newArgs);
+        }
+      }
+      return originalFetch.call(unsafeWindow, resource, ...restArgs);
+    });
+  }
+
   mockImplementation(unsafeWindow, 'Request', ([url, opts, ...rest], originalRequest) => {
     try {
       const rurl = new URL(url);
       if (rurl.pathname === unsafeWindow.W.Config.paths.updateRequestComments) {
         const origText = opts.body.get('text');
-        if (!origText.endsWith(SIGNATURE)) opts.body.set('text', origText + SIGNATURE);
+        if (!origText.endsWith(SIGNATURE)) opts.body.set('text', origText + FULL_SIG);
       }
     }
     finally {
       return new originalRequest(url, opts, ...rest);
     }
   });
+
+  // mockFetchRequest(unsafeWindow.W.Config.paths.updateRequestComments, (request, ...otherArgs) => {
+  //   console.log(args);
+  // })
 };
 
 if (unsafeWindow.W?.userscripts?.state.isInitialized) {
